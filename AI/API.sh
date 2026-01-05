d22f483329cd6cb00de264b8a934222f7a1dd3ce
deymosalmut <deymosalmut@gmail.com>
2026-01-05 18:07:00 +0300

    235432532532553

---
+++ AI/API.sh
@@ -0,0 +1,2 @@
+unset OPENAI_API_KEY
+export OPENAI_API_KEY="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"
